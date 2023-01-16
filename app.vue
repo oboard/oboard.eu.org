bd8a6045872a80bc95f788d2f5c2de0764e2f7e5
oboard <oboard@outlook.com>
2023-01-16 10:03:19 +0000

    pwa

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="p-4">
-    <button class="btn">Hello daisyUI</button>
+    <button class="btn">Hello daisyU233I1</button>
   </div>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,20 @@
 export default {
-    modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/pwa"],
+  pwa: {
+    icon: {
+      source: "/icon2.png", //路径为static中的icon2.png
+      fileName: "icon2.png",
+    },
+
+    manifest: {
+      name: "oboard",
+      short_name: "oboard",
+      lang: "zh-CN",
+      theme_color: "#fff",
+      description: "一块小板子",
+    },
+    // workbox: {        //开发环境取消注释,调试pwa, 打包时注释
+    //   dev: true
+    // }
+  },
 };
