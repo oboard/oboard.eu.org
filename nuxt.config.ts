c1b0b08d4f067c7a7dd4d4e12d221290581b193a
oboard <oboard@outlook.com>
2023-01-20 20:06:10 +0800

    ignore change

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "一块小板子",
+      meta: [
+        { name: "description", content: "一块小板子的主页" },
+        { name: "keyword", content: "一块小板子" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "favicon.ico" }],
+    },
+  },
+});
